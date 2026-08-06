5c67b13307cd5bfe88f5163b7aa179c2774c9c33
oliviawyy <olivia.c@aluno.senai.br>
2026-08-06 11:30:07 -0300

    gwr

--- src/components/ConsultaApiBtn/ConsultaApiSa.jsx
+++ src/components/ConsultaApiBtn/ConsultaApiSa.jsx
@@ -23,28 +23,28 @@ function ConsultaApiBtn() {
         setCarregando(true);
         setErro("");
 
-        try{
+        try {
             // Url para simular um erro interno do servidor (401 e 500)
             // const resposta = await fetch("https://httpbin.org/status/401");
 
             // consultar a API utilizando o metodo fetch --> resposta é uma promisse
-            const resposta = await fetch("https://jsonplaceholder.typicode.com/user");
+            const resposta = await fetch("https://jsonplaceholder.typicode.com/users");
             console.log(resposta)
 
             if (!resposta.ok) {
 
-            // tratar erro interno do servidor (500)
-            if (resposta.status === 500){
-                throw new Error("Erro 500: O banco de dados ou servidor falhou.");
-            }
+                // tratar erro interno do servidor (500)
+                if (resposta.status === 500) {
+                    throw new Error("Erro 500: O banco de dados ou servidor falhou.");
+                }
 
-            // tratar erro 401 (falta de autenticação)
-            if (resposta.status === 401){
-                throw new Error("Erro 401: Usuário não autorizado.");
-            }
+                // tratar erro 401 (falta de autenticação)
+                if (resposta.status === 401) {
+                    throw new Error("Erro 401: Usuário não autorizado.");
+                }
 
-            // tratar erro HTTP do servidor (URL 404)
-            throw new Error(`Erro ${resposta.status}: URL não encontrada ou inválida.`);
+                // tratar erro HTTP do servidor (URL 404)
+                throw new Error(`Erro ${resposta.status}: URL não encontrada ou inválida.`);
 
             }
 
@@ -61,58 +61,65 @@ function ConsultaApiBtn() {
                 setErro("Não foi possível concectar ao servidor. Verifique sua internet.");
 
 
-        } 
-        else {
-            setErro(error.message);
-        }}
+            }
+            else {
+                setErro(error.message);
+            }
+        }
         finally {
             setCarregando(false);
         }
 
     }
 
-    return( 
-        <main className={styles.container}> 
+    return (
+        <main className={styles.container}>
 
-         <h1 className={styles.titulo}>Consulta API</h1>
+            <h1 className={styles.titulo}>Consulta API</h1>
 
-        <section className={styles.card}>
+            <section className={styles.card}>
 
-        <button 
-        className={styles.buscarButton} 
-        onClick={buscarUsuarios} 
-        disabled={carregando}
-        >
-            {carregando ? "Buscando..." : "Consultar"}
-        </button>
+                <button
+                    className={styles.buscarButton}
+                    onClick={buscarUsuarios}
+                    disabled={carregando}
+                >
+                    {carregando ? "Buscando..." : "Consultar"}
+                </button>
 
-        <h2>Usuários da JSON Placeholder</h2>
+                <h2>Usuários da JSON Placeholder</h2>
 
-        {/* exibe mensagem de carregamento */}
-        {carregando &&<p>Carregando usuários...</p>}
+                {/* exibe mensagem de carregamento */}
+                {carregando && Swal.fire({
+                    title: "Sucesso!",
+                    text: "Usuários carregados com sucesso.",
+                    icon: "success"
+                })
 
-        {/* exibe mensagem de erro */}
-        {erro && <p className={styles.erro}>{erro}</p>}
+}
 
-        {/* exibe a lista de usuários */}
-        {!carregando && !erro && (
-            <ul className={styles.lista}>
-                {usuarios.map((usuario) => (
-                    <li key={usuario.id} className={styles.item}>
-                        <h3>{usuario.name}</h3>
-                        <p>
-                            <strong>E-mail</strong> {usuario.email}
-                        </p>
-                        <p>
-                            <strong>Cidade:</strong> {usuario.address.city}
-                        </p>
-                        </li>
-                ))}
-            </ul>
-        )}
+                {/* exibe mensagem de erro */}
+                {erro && <p className={styles.erro}>{erro}</p>}
 
-    
-        </section>
+                {/* exibe a lista de usuários */}
+                {!carregando && !erro && (
+                    <ul className={styles.lista}>
+                        {usuarios.map((usuario) => (
+                            <li key={usuario.id} className={styles.item}>
+                                <h3>{usuario.name}</h3>
+                                <p>
+                                    <strong>E-mail</strong> {usuario.email}
+                                </p>
+                                <p>
+                                    <strong>Cidade:</strong> {usuario.address.city}
+                                </p>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+
+
+            </section>
         </main>
     )
 }
